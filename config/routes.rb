3b8f01746d98a35061b3db16a15465b5bacac951
Vinicius Coelho <vinicius2001@gmail.com>
2023-06-06 20:47:30 -0300

    starting dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
   resources :likeds
   resources :comments
   resources :posts
   resources :community_participants
   resources :communities
+  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
