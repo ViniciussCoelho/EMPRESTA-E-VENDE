cb671ddd737c01d4880336f9fcf730a69f422d65
Vinicius Coelho <vinicius2001@gmail.com>
2023-06-19 20:55:52 -0300

    add - show community front

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
