0587c06d2da295d37a62a1de462df59f513c639b
Vinicius Coelho <vinicius2001@gmail.com>
2023-06-24 20:35:28 -0300

    show post complete

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,15 +1,2 @@
 module DashboardHelper
-    def resource_image(resource)
-        if resource.image.attached?
-            resource.image
-        else
-            if resource.is_a?(Community)
-                'community_default.png'
-            elsif resource.is_a?(User)
-                'community_default.png'
-            elsif resource.is_a?(Post)
-                'community_default.png'
-            end
-        end
-    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :community_participant
   has_many :likeds, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 end
